d07d1a36cf776b95c995b2c19cad97f9a2f27360
maahyarr <mahyar.amini1996@gmail.com>
2026-01-07 12:52:54 +0330

    add

--- src/components/Sections/AboutSection.jsx
+++ src/components/Sections/AboutSection.jsx
@@ -1,48 +1,86 @@
-import React from 'react'
-import Bar from '../Ui/Bar'
+import React from "react";
+import Bar from "../Ui/Bar";
+import stat1 from "/images/count3/1.png";
+import stat2 from "/images/count3/2.png";
+import stat3 from "/images/count3/3.png";
+import stat4 from "/images/count3/4.png";
 
 export default function AboutSection() {
+  const statsCards = [
+    {
+      id: 1,
+      image: stat1,
+      title: "مشتریان گلستان",
+    },
+    {
+      id: 2,
+      image: stat2,
+      title: "محصولات گلستان",
+    },
+    {
+      id: 3,
+      image: stat3,
+      title: "مراكز فروش",
+    },
+    {
+      id: 4,
+      image: stat4,
+      title: "سرمایه انسانی",
+    },
+  ];
+
   return (
     <>
-    <Bar/>
-    <div className="pt-8 lg:pt-12 max-w-400 min-h-155  mx-auto">
-
+      <Bar />
+      <div className="pt-8 lg:pt-12 max-w-400 min-h-155  mx-auto">
         <div className="mx-auto max-w-175 px-4 lg:px-0 text-right animate__animated animate__fadeInRightBig">
-          <h2 className="font-IRANSans text-[18px] font-bold leading-5 text-[#ed1c24] pt-4">معرفی گلستان</h2>
+          <h2 className="font-IRANSans text-[18px] font-bold leading-5 text-[#ed1c24] pt-4">
+            معرفی گلستان
+          </h2>
           <h2 className="font-IRANSans text-[15px] font-thin leading-[36px] text-[#7a7a7a] text-justify pt-4">
-            آنچه که امروز با نام مجموعه <span className="text-golestan-red">گلستان</span> می‌شناسیم در سال 1329 بنیان نهاده شد. زمانی که شرکت گلستان فعالیت خود را با بسته‌بندی و توزیع چای پاکتی آغاز کرد. با گذر زمان و رشد اقتصاد کشور، نهال «گلستان» به تدریج تقویت شد و شاخ و برگ گرفت. فعالیت‌های «گلستان» با تهیه، تولید و بسته‌بندی بهترین محصولات غذایی گسترش پیدا کرد.
-            <span className="text-golestan-red">گلستان</span> پس از سال‌ها تلاش همچنان می‌کوشد تا به کمک تجربه‌های اندوخته شده در این سال‌ها کالاهایی قابل اعتماد و با کیفیت تولید نماید.
+            آنچه که امروز با نام مجموعه{" "}
+            <span className="text-golestan-red">گلستان</span> می‌شناسیم در سال
+            1329 بنیان نهاده شد. زمانی که شرکت گلستان فعالیت خود را با بسته‌بندی
+            و توزیع چای پاکتی آغاز کرد. با گذر زمان و رشد اقتصاد کشور، نهال
+            «گلستان» به تدریج تقویت شد و شاخ و برگ گرفت. فعالیت‌های «گلستان» با
+            تهیه، تولید و بسته‌بندی بهترین محصولات غذایی گسترش پیدا کرد.
+            <span className="text-golestan-red">گلستان</span> پس از سال‌ها تلاش
+            همچنان می‌کوشد تا به کمک تجربه‌های اندوخته شده در این سال‌ها
+            کالاهایی قابل اعتماد و با کیفیت تولید نماید.
           </h2>
           <h2 className="font-IRANSans text-[16px] font-thin leading-9 text-[#7a7a7a] text-justify pt-4">
-            هدفمندی و برنامه‌ریزی موثر، ایمان و عزم راسخ برای دستیابی به اهداف تعیین شده، به کار گیری همکاران مجرب و دلسوز، استفاده از روش های نوین بازاریابی و مشتری محوری از جمله عوامل مهم در استراتژی
+            هدفمندی و برنامه‌ریزی موثر، ایمان و عزم راسخ برای دستیابی به اهداف
+            تعیین شده، به کار گیری همکاران مجرب و دلسوز، استفاده از روش های نوین
+            بازاریابی و مشتری محوری از جمله عوامل مهم در استراتژی
             <span className="text-golestan-red">شرکت گلستان</span> است.
           </h2>
-          <div className="font-IRANSans text-[20px] font-bold leading-5 text-[#ed1c24] pt-4">محصولات گلستان​</div>
+          <div className="font-IRANSans text-[20px] font-bold leading-5 text-[#ed1c24] pt-4">
+            محصولات گلستان​
+          </div>
           <p className="font-IRANSans text-[16px] font-thin leading-9 text-[#7a7a7a] text-justify pt-4">
-            <span className="text-golestan-red font-bold"> نام تجاری گلستان</span> قدمتی به اندازه عمر شرکت گلستان دارد. گلستان ممتازترین محصولات خود را با این نام تجاری عرضه می‌کند. مصرف‌کنندگان محصولات گلستان همواره از این نام تجاری خاطره محصولاتی ممتاز و منحصر بفرد دارند و با اطمینان از نهایت کیفیت این نام تجاری حمایت می‌کنند.
+            <span className="text-golestan-red font-bold">
+              {" "}
+              نام تجاری گلستان
+            </span>{" "}
+            قدمتی به اندازه عمر شرکت گلستان دارد. گلستان ممتازترین محصولات خود
+            را با این نام تجاری عرضه می‌کند. مصرف‌کنندگان محصولات گلستان همواره
+            از این نام تجاری خاطره محصولاتی ممتاز و منحصر بفرد دارند و با
+            اطمینان از نهایت کیفیت این نام تجاری حمایت می‌کنند.
           </p>
         </div>
-    </div>
-    <div className="hidden md:flex flex-row-reverse flex-nowrap justify-center max-w-400 mx-auto min-h-72.5  mt-8 lg:mt-12 mb-32 lg:mb-48 animate__animated animate__zoomIn">
-             
-        <div className=" m-2 lg:m-4 flex flex-col flex-nowrap justify-around items-center border-b border-b-[#dbdbdb]">
-          <img src="images/count3/1.png" alt="" className=" w-40 lg:w-48" />
-          <p className="font-IRANSans text-[16px] font-thin leading-5.25 text-[#7c7d7e] text-center mt-2.5">مشتریان گلستان</p>
-        </div>
-        <div className=" m-2 lg:m-4 flex flex-col flex-nowrap justify-around items-center border-b border-b-[#dbdbdb]">
-          <img src="images/count3/2.png" alt="" className=" w-40 lg:w-48" />
-          <p className="font-IRANSans text-[16px] font-thin leading-5.25 text-[#7c7d7e] text-center mt-2.5">محصولات گلستان</p>
-        </div>
-        <div className=" m-2 lg:m-4 flex flex-col flex-nowrap justify-around items-center border-b border-b-[#dbdbdb]">
-          <img src="images/count3/3.png" alt="" className=" w-40 lg:w-48" />
-          <p className="font-IRANSans text-[16px] font-thin leading-5.25 text-[#7c7d7e] text-center mt-2.5">مراكز فروش</p>
-        </div>
-        <div className=" m-2 lg:m-4 flex flex-col flex-nowrap justify-around items-center border-b border-b-[#dbdbdb]">
-          <img src="images/count3/4.png" alt="" className=" w-40 lg:w-48" />
-          <p className="font-IRANSans text-[16px] font-thin leading-5.25 text-[#7c7d7e] text-center mt-2.5">سرمایه انسانی</p>
-        </div>
-      
-    </div>
+      </div>
+      <div className="hidden md:flex flex-row-reverse flex-nowrap justify-center max-w-400 mx-auto min-h-72.5  mt-8 lg:mt-12 mb-32 lg:mb-48 animate__animated animate__zoomIn">
+        {statsCards.map((item) => {
+          return (
+            <div key={item.id} className=" m-2 lg:m-4 flex flex-col flex-nowrap justify-around items-center border-b border-b-[#dbdbdb]">
+              <img src={item.image} alt="" className=" w-40 lg:w-48" />
+              <p className="font-IRANSans text-[16px] font-thin leading-5.25 text-[#7c7d7e] text-center mt-2.5">
+                {item.title}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
--- src/components/Sections/ProductShowcase.jsx
+++ src/components/Sections/ProductShowcase.jsx
@@ -1,106 +1,112 @@
-import React from 'react'
-import Bar from '../Ui/Bar'
-import Button from '../Ui/Button'
-
+import React from "react";
+import Bar from "../Ui/Bar";
+import Button from "../Ui/Button";
+import riceImage from "/images/count4/1.jpg";
+import teaImage from "/images/count6/1.jpg";
+import nutsImage from "/images/count8/1.jpg";
 export default function ProductShowcase() {
+  const products = [
+    {
+      id: 1,
+      image: riceImage,
+      title: "برنج گلستان",
+      subtitle: "معرفی محصول",
+      heading: "رکن اول غذای ایرانی",
+      description: 
+        "مرغوب‌ترین برنج دنیا، برنج ایران است و بهترین نوع برنج ایرانی،‌ طارم و هاشمی. این برنج‌ها از بهترین شالیزارهای مازندران برداشت شده، پس از كنترل كیفیت در آزمایشگاه‌های تخصصی گلستان وارد چرخه بسته‌بندی و در وزن‌های مختلف به بازار عرضه می‌شوند. عطر و طعم بی‌نظیر برنج گلستان با هیچ برنج دیگری قابل مقایسه نیست. با گلستان،‌ كیفیت زندگی بالاتر از همیشه است چرا كه مشتریان گلستان؛‌ لایق بهترین‌ها هستند.",
+      
+      buttons: [{ text: "محصولات برنج" }, { text: "آشپزخانه گلستان" }],
+    },
+    {
+      id: 2,
+      image: teaImage,
+      title: "چای گلستان",
+      subtitle: "معرفی محصول",
+      heading: "سابقه‌ای به قدمت یک عمر",
+      description: 
+        <>
+        چای تنها یك نوشیدنی گرم نیست.‌ یک گفتمان است؛‌ گرمابخش وجود و بهانه‌ای برای آغاز یك معاشرت دل‌چسب. بی‌دلیل نیست كه نام «محبوب‌ترین نوشیدنی دنیا» را با خود یدک می‌كشد. نوشیدنش خستگی‌ها را فراری داده،‌ حس خوب آرامش را به همراه دارد.,
+        
+        <br/>
+        <br/>
+          برای مردمان ایران زمین نام
+          <span className="text-[#ed1c24] font-bold">گلستان</span> با چای پیوند
+          خورده است؛‌ قدیمی‌ترین محصول مجموعه گلستان،‌ با مدرن‌ترین و
+          بهداشتی‌ترین دستگاه‌های روز دنیا تهیه و بسته‌بندی می‌شود. چیزی كه باعث
+          شده مشتریانش یك عمر به نام گلستان اعتماد كنند، كیفیت، ‌عطر و طعم به
+          یادماندنی آن است.
+        </>,
+      
+      buttons: [{ text: "محصولات چای" }],
+    },
+    {
+      id: 3,
+      image: nutsImage,
+      title: "آجیل گلستان",
+      subtitle: "معرفی محصول",
+      heading: "آجيل‌های خوش خنده",
+      description: 
+        <>
+        "خواص فوق‌العاده پسته بر كسی پوشیده نیست. پسته را منبع ویتامین و مواد معدنی می‌نامند، یك پمپ خون‌ساز قوی و تسكین دهنده قلب و اعصاب.",
+        
+        <br />
+        <br />
+         <span className="text-[#ed1c24] font-bold">پسته گلستان</span> اما حسابش از دیگر پسته‌های بازار جداست. مرغوب‌ترین پسته خام از بهترین باغ‌های پسته ایران دستچین می‌شود.‌ پس از كنترل كیفیت در آزمایشگاه و اطمینان از سلامت محصول،‌ با زعفران درجه یک و نمک، بوداده و برشته شده و‌ پسته گلستان را با طعم و رنگ و عطری بی‌نظیر به ارمغان می‌آورد.
+        </>,
+      
+      buttons: [{ text: "ناتس گلستان" }],
+    },
+  ];
+
   return (
     <>
-    <Bar/>
-    <div className="relative mb-8 lg:mb-12 max-w-400 mx-auto">
-        <div className="c4-box1 w-full md:min-h-125">
-          <img src="images/count4/1.jpg" alt="" className=" w-full h-60.25 md:min-h-125 object-right md:object-cover" />
-        </div>
-        
-        <div className='max-w-400 h-6 bg-[#e9d69f] relative mx-auto'>
-        <div className="bg-[#ed1c24] w-full md:w-[289px] md:h-[93px] absolute bottom-0 md:right-[20%] text-right">
-          <h3 className="font-IRANSans text-[29px] font-thin leading-[44px] text-white text-start pr-10 pe-4">برنج گلستان</h3>
-          <span className="font-IRANSans text-[16px] font-thin leading-[44px] pr-10 text-white text-end pe-4">معرفی محصول</span>
-        </div>
-        </div>
-      </div>
-      <Bar/>
-        <div className=" pt-5 pe-4 max-w-400 mx-auto">
-        <div className="mx-auto max-w-175 px-4 lg:px-0 text-right">
-          <h2 className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4">رکن اول غذای ایرانی</h2>
-          <p className="font-IRANSans text-[16px] font-thin leading-[36px] text-[#7a7a7a] text-justify pt-4">
-            مرغوب‌ترین برنج دنیا، برنج ایران است و بهترین نوع برنج ایرانی،‌ طارم و هاشمی. این برنج‌ها از بهترین شالیزارهای مازندران برداشت شده، پس از كنترل كیفیت در آزمایشگاه‌های تخصصی گلستان وارد چرخه بسته‌بندی و در وزن‌های مختلف به بازار عرضه می‌شوند. عطر و طعم بی‌نظیر برنج گلستان با هیچ برنج دیگری قابل مقایسه نیست. با گلستان،‌ كیفیت زندگی بالاتر از همیشه است چرا كه مشتریان گلستان؛‌ لایق بهترین‌ها هستند.
-          </p>
-          <div className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4 pb-5 mb-4 flex flex-row flex-wrap gap-2 lg:gap-4">
-            <Button>
-                  محصولات برنج
-            </Button>
-            <Button>
-                آشپزخانه گلستان
-            </Button>
-           
-           
-          </div>
-        </div>
-      </div>
-    <div className="relative mb-8 lg:mb-12 max-w-400 mx-auto">
-        <div className="c4-box1 w-full md:min-h-125">
-          <img src="images/count4/1.jpg" alt="" className=" w-full h-60.25 md:min-h-125 object-right md:object-cover" />
-        </div>
-        
-        <div className='max-w-400 h-6 bg-[#e9d69f] relative mx-auto'>
-        <div className="bg-[#ed1c24] w-full md:w-[289px] md:h-[93px] absolute bottom-0 md:right-[20%] text-right">
-          <h3 className="font-IRANSans text-[29px] font-thin leading-[44px] text-white text-start pr-10 pe-4">برنج گلستان</h3>
-          <span className="font-IRANSans text-[16px] font-thin leading-[44px] pr-10 text-white text-end pe-4">معرفی محصول</span>
-        </div>
-        </div>
-      </div>
-      <Bar/>
-        <div className=" pt-5 pe-4 max-w-400 mx-auto">
-        <div className="mx-auto max-w-175 px-4 lg:px-0 text-right">
-          <h2 className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4">رکن اول غذای ایرانی</h2>
-          <p className="font-IRANSans text-[16px] font-thin leading-[36px] text-[#7a7a7a] text-justify pt-4">
-            مرغوب‌ترین برنج دنیا، برنج ایران است و بهترین نوع برنج ایرانی،‌ طارم و هاشمی. این برنج‌ها از بهترین شالیزارهای مازندران برداشت شده، پس از كنترل كیفیت در آزمایشگاه‌های تخصصی گلستان وارد چرخه بسته‌بندی و در وزن‌های مختلف به بازار عرضه می‌شوند. عطر و طعم بی‌نظیر برنج گلستان با هیچ برنج دیگری قابل مقایسه نیست. با گلستان،‌ كیفیت زندگی بالاتر از همیشه است چرا كه مشتریان گلستان؛‌ لایق بهترین‌ها هستند.
-          </p>
-          <div className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4 pb-5 mb-4 flex flex-row flex-wrap gap-2 lg:gap-4">
-            <Button>
-                  محصولات برنج
-            </Button>
-            <Button>
-                آشپزخانه گلستان
-            </Button>
-           
-           
-          </div>
-        </div>
-      </div>
-    <div className="relative mb-8 lg:mb-12 max-w-400 mx-auto">
-        <div className="c4-box1 w-full md:min-h-125">
-          <img src="images/count4/1.jpg" alt="" className=" w-full h-60.25 md:min-h-125 object-right md:object-cover" />
-        </div>
-        
-        <div className='max-w-400 h-6 bg-[#e9d69f] relative mx-auto'>
-        <div className="bg-[#ed1c24] w-full md:w-[289px] md:h-[93px] absolute bottom-0 md:right-[20%] text-right">
-          <h3 className="font-IRANSans text-[29px] font-thin leading-[44px] text-white text-start pr-10 pe-4">برنج گلستان</h3>
-          <span className="font-IRANSans text-[16px] font-thin leading-[44px] pr-10 text-white text-end pe-4">معرفی محصول</span>
-        </div>
-        </div>
-      </div>
-      <Bar/>
-        <div className=" pt-5 pe-4 max-w-400 mx-auto">
-        <div className="mx-auto max-w-175 px-4 lg:px-0 text-right">
-          <h2 className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4">رکن اول غذای ایرانی</h2>
-          <p className="font-IRANSans text-[16px] font-thin leading-[36px] text-[#7a7a7a] text-justify pt-4">
-            مرغوب‌ترین برنج دنیا، برنج ایران است و بهترین نوع برنج ایرانی،‌ طارم و هاشمی. این برنج‌ها از بهترین شالیزارهای مازندران برداشت شده، پس از كنترل كیفیت در آزمایشگاه‌های تخصصی گلستان وارد چرخه بسته‌بندی و در وزن‌های مختلف به بازار عرضه می‌شوند. عطر و طعم بی‌نظیر برنج گلستان با هیچ برنج دیگری قابل مقایسه نیست. با گلستان،‌ كیفیت زندگی بالاتر از همیشه است چرا كه مشتریان گلستان؛‌ لایق بهترین‌ها هستند.
-          </p>
-          <div className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4 pb-5 mb-4 flex flex-row flex-wrap gap-2 lg:gap-4">
-            <Button>
-                  محصولات برنج
-            </Button>
-            <Button>
-                آشپزخانه گلستان
-            </Button>
-           
-           
-          </div>
-        </div>
-      </div>
+      <Bar />
+      {products.map((product) => {
+        return (
+          <React.Fragment key={product.id}>
+            <div className="relative mb-8 lg:mb-12 max-w-400 mx-auto">
+              <div className="c4-box1 w-full md:min-h-125">
+                <img
+                  src={product.image}
+                  alt=""
+                  className=" w-full h-60.25 md:min-h-125 object-right md:object-cover"
+                />
+              </div>
+
+              <div className="max-w-400 h-6 bg-[#e9d69f] relative mx-auto">
+                <div className="bg-[#ed1c24] w-full md:w-[289px] md:h-[93px] absolute bottom-0 md:right-[20%] text-right">
+                  <h3 className="font-IRANSans text-[29px] font-thin leading-[44px] text-white text-start pr-10 pe-4">
+                    {product.title}
+                  </h3>
+                  <span className="font-IRANSans text-[16px] font-thin leading-[44px] pr-10 text-white text-end pe-4">
+                   {product.subtitle}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <Bar />
+            <div className=" pt-5 pe-4 max-w-400 mx-auto">
+              <div className="mx-auto max-w-175 px-4 lg:px-0 text-right">
+                <h2 className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4">
+                 {product.heading}
+                </h2>
+                <p className="font-IRANSans text-[16px] font-thin leading-[36px] text-[#7a7a7a] text-justify pt-4">
+                  {product.description}
+                </p>
+                <div className="font-IRANSans text-[20px] font-bold leading-[20px] text-[#ed1c24] pt-4 pb-5 mb-4 flex flex-row flex-wrap gap-2 lg:gap-4">
+                              {product.buttons.map((button, index) => (
+                  <Button key={button.text}>
+                    {button.text}
+                  </Button>
+                ))}
+                </div>
+              </div>
+            </div>
+           </React.Fragment>
+        );
+      })}
+
       <div className="max-w-400 mx-auto bg-[url('images/count10/recipe-baner-1.png')] bg-center bg-fixed bg-cover h-[250px]"></div>
-    
     </>
-  )
+  );
 }
